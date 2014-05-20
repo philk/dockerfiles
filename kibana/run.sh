c60c4226738d694b9ed4c7f65b88451848211246
Phil Kates <me@philkates.com>
2014-05-19 17:43:22 -0700

    kibana: Use the same protocol in kibana
    This way if we're terminating SSL somewhere else like a load balancer it
    doesn't try to access elasticsearch over a non-existent http

--- kibana/run.sh
+++ kibana/run.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/%es_upstream%/$ES_UPSTREAM/g" /etc/nginx/conf.d/kibana.conf
 sed -i "s/%server_name%/$SERVER_NAME/g" /etc/nginx/conf.d/kibana.conf
-sed -i "/    elasticsearch:.*/c elasticsearch: \"http://\"\+window\.location\.hostname," /opt/kibana/config.js
+sed -i "/    elasticsearch:.*/c elasticsearch: window.location.protocol+\"//\"+window.location.hostname," /opt/kibana/config.js
 
 nginx -t -c /etc/nginx/nginx.conf
 nginx -c /etc/nginx/nginx.conf
